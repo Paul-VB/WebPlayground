a4d071fd2f1af3c9e03a65836ec81dc8acea83c1
Paul Vanden Broeck <PVB96P@gmail.com>
2025-07-28 18:42:42 -0400

    adding peter griffin

--- web-playground-react/src/pages/Home.jsx
+++ web-playground-react/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import peterGriffin from '../assets/peter_griffin.png';
 
 function Home() {
 	return (
 		<>
 			<h1>Home</h1>
 			This is a simple React home page.
+			<img src={peterGriffin} alt="Peter Griffin" />
 		</>
 	);
 }
