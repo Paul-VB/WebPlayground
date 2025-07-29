0fcd425268e62065754aedbad2811966d168899a
Paul Vanden Broeck <PVB96P@gmail.com>
2025-07-29 16:40:06 -0400

    adding Saas and updating some styles for the home page

--- web-playground-react/src/pages/Home.jsx
+++ web-playground-react/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Home.css';
+import './Home.scss';
 import abe from '../assets/abe.jpg';
 
 function Home() {
 	return (
-		<>
+		<div className="home">
 			<div className="headshot-container">
 				<img src={abe} alt="Placeholder Mugshot" className="headshot" />
 			</div>
 			<h1>Paul Vanden Broeck</h1>
 			This is a simple React home page.
-		</>
+		</div>
 	);
 }
 
