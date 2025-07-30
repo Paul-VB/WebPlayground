36c9a7b65a10465be46453d5e81aae1245c38d98
Paul Vanden Broeck <PVB96P@gmail.com>
2025-07-30 11:18:30 -0400

    removing routes.js as we already have a jsx file

--- web-playground-react/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './pages/Home';
-import About from './pages/About';
-import Ai from './pages/Ai';
-
-export const ROUTES = {
-  home: { path: '/', name: 'Home', component: <Home /> },
-  about: { path: '/about', name: 'About', component: <About /> },
-  ai: { path: '/ai', name: 'Ai', component: <Ai /> }
-};
