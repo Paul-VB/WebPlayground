789a6a0f72150dbc7e5ded722cfacceeb1a10100
Paul Vanden Broeck <PVB96P@gmail.com>
2025-08-13 12:15:25 -0400

    refactoring api program.cs to have main method and supporting setup methods

--- WebPlayground.Api/Program.cs
+++ WebPlayground.Api/Program.cs
@@ -1,45 +1,59 @@
 using WebPlayground.Api;
 
-var builder = WebApplication.CreateBuilder(args);
-
-ApiServiceRegistrar.RegisterServices(builder.Services, builder.Configuration);
-
-builder.Services.AddCors(options =>
+public class Program
 {
-    options.AddPolicy("DevelopmentCorsPolicy",
-        policy =>
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        ApiServiceRegistrar.RegisterServices(builder.Services, builder.Configuration);
+        ConfigureCors(builder.Services);
+
+        var app = builder.Build();
+        ConfigureMiddleware(app);
+
+        app.Run();
+    }
+
+    private static void ConfigureCors(IServiceCollection services)
+    {
+        services.AddCors(options =>
         {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+            options.AddPolicy("DevelopmentCorsPolicy",
+                policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
 
-    options.AddPolicy("ProductionCorsPolicy",
-        policy =>
+            options.AddPolicy("ProductionCorsPolicy",
+                policy =>
+                {
+                    policy.WithOrigins(
+                            "https://black-plant-047accb0f.1.azurestaticapps.net"
+                        )
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
         {
-            policy.WithOrigins(
-                    "https://black-plant-047accb0f.1.azurestaticapps.net"
-                )
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseCors("DevelopmentCorsPolicy");
+        }
+        else
+        {
+            app.UseCors("ProductionCorsPolicy");
+        }
 
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseCors("DevelopmentCorsPolicy");
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
 }
-else
-{
-    app.UseCors("ProductionCorsPolicy");
-}
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
